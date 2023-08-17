0e80a2c35bd43e85aa4250a3628359660a332f57
realtofuine <73127480+realtofuine@users.noreply.github.com>
2023-08-17 10:14:03 -0500

    cleanup string name

--- CaltoNotion.py
+++ CaltoNotion.py
@@ -13,16 +13,20 @@ webcal = os.environ["WEB_CAL"]
 notion = Client(auth=os.environ["NOTION_TOKEN"])
 
 nameList = []
+editedNameList = []
 assignList = []
 dueList = []
+courseList = []
 notionNameList = []
 notionAssignList = []
 notionDueList = []
 notionIDList = []
 
 nameList.clear()
+editedNameList.clear()
 assignList.clear()
 dueList.clear()
+courseList.clear()
 notionNameList.clear()
 notionAssignList.clear()
 notionDueList.clear()
@@ -49,6 +53,44 @@ for event in cal.walk('vevent'):
         dueList.append(str((dateend)))
     else: #If dateend is a date object, convert to datetime and subtract one day (there is an offset for some reason)
         dueList.append(str((dateend - timedelta(days = 1))))
+
+# Create editedNameList to remove name prefix and courseList to add course name
+for name in nameList:
+    if("AP Physics C" in name):
+            course = "AP Phy C"
+            editedNameList.append(name[56:])
+            print(course)
+            courseList.append(course)
+    elif("AP United States History" in name):
+        course = "APUSH"
+        editedNameList.append(name[30:])
+        print(course)
+        courseList.append(course)
+    elif("Advanced Topics in Mathematics" in name):
+        course = "AT"
+        editedNameList.append(name[40:])
+        print(course)
+        courseList.append(course)
+    elif("AP Chemistry" in name):
+        course = "AP Chem"
+        editedNameList.append(name[18:])
+        print(course)
+        courseList.append(course)
+    elif("English 12 - Literature of War" in name):
+        course = "Eng"
+        editedNameList.append(name[47:])
+        print(course)
+        courseList.append(course)
+    elif("AP Computer Science A" in name):
+        course = "APCSA"
+        editedNameList.append(name[27:])
+        print(course)
+        courseList.append(course)
+    else:
+        course = "Not Found"
+        editedNameList.append(name)
+        print(course)
+        courseList.append(course)
 
 # Read Notion data and save name field to list
 my_page = notion.databases.query(
@@ -82,12 +124,13 @@ for result in my_page:
     notionDueList.append(str(result['properties']['Due']['date']['start']))
     notionIDList.append(str(result['id']))
 
+
 # If event is in both lists, compare dates and update Notion if necessary
 def getmatchingindex(j):
     for i in range(len(notionNameList)):
-        if nameList[j] == notionNameList[i]:
+        if editedNameList[j] == notionNameList[i]:
             print("Match found")
-            print(nameList[j])
+            print(editedNameList[j])
             print(notionNameList[i])
             print(assignList[j])
             print(notionAssignList[i])
@@ -120,30 +163,9 @@ def getmatchingindex(j):
 
 # Compare lists and add new events to Notion
 
-for i in range(len(nameList)):
-    if nameList[i] not in notionNameList:
-        print("New event found: " + nameList[i])
-        if("AP Physics C" in nameList[i]):
-            course = "AP Phy C"
-            print(course)
-        elif("AP United States History" in nameList[i]):
-            course = "APUSH"
-            print(course)
-        elif("Advanced Topics in Mathematics" in nameList[i]):
-            course = "AT"
-            print(course)
-        elif("AP Chemistry" in nameList[i]):
-            course = "AP Chem"
-            print(course)
-        elif("English 12" in nameList[i]):
-            course = "Eng"
-            print(course)
-        elif("AP Computer Science A" in nameList[i]):
-            course = "APCSA"
-            print(course)
-        else:
-            course = "None"
-            print(course)
+for i in range(len(editedNameList)):
+    if editedNameList[i] not in notionNameList:
+        print("New event found: " + editedNameList[i])
 
         newPage = notion.pages.create(
             parent={
@@ -154,7 +176,7 @@ for i in range(len(nameList)):
                     "title": [
                         {
                             "text": {
-                                "content": nameList[i]
+                                "content": editedNameList[i]
                             }
                         }
                     ]
@@ -171,7 +193,7 @@ for i in range(len(nameList)):
                 },
                 "Class": {
                     "select": {
-                        "name": course
+                        "name": courseList[i]
                     }
                 }
             }
@@ -184,7 +206,7 @@ for i in range(len(nameList)):
 #Remove events from Notion if they are not in the calendar
 
 for i in range(len(notionNameList)):
-    if notionNameList[i] not in nameList:
+    if notionNameList[i] not in editedNameList:
         # Check if item has checkbox checked
         if notion.pages.retrieve(page_id = notionIDList[i])['properties']['Manually Added']['checkbox'] == True:
             print("Canceled removing")
